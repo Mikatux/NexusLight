9a0c6efd67f1dfacd860f7d8cc47787c182295ac
Mickael MAYEUR <mickael@mayeur.eu>
2015-11-04 02:18:31 +0100

    Add websocket begining and record begining

--- app/src/main/java/eu/mayeur/mickael/nexuslight/MainActivity.java
+++ app/src/main/java/eu/mayeur/mickael/nexuslight/MainActivity.java
@@ -14,72 +14,187 @@
 
 package eu.mayeur.mickael.nexuslight;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.hoho.android.usbserial.driver.UsbSerialDriver;
-import com.hoho.android.usbserial.driver.UsbSerialPort;
-import com.hoho.android.usbserial.driver.UsbSerialProber;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /*
  * MainActivity class that loads MainFragment
  */
 public class MainActivity extends Activity {
+    private static final int REQUEST_CODE = 1;
+    private boolean wsOpen = false;
+
     /**
      * Called when the activity is first created.
      */
+    private MediaProjectionManager mMediaProjectionManager;
+    private ScreenRecorder mRecorder;
+    WebSocketClient mWebSocketClient;
+
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p/>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
 
+        Button connect = (Button) findViewById(R.id.bt_connect);
+        EditText ipView = (EditText) findViewById(R.id.et_ip);
+        final String ip = ipView.getText().toString();
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!wsOpen){
+                connectWebSocket(ip);
+                }
+                else{
+                    mWebSocketClient.close();
+                }
 
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty()) {
-            return;
-        }
-
-// Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == null) {
-            // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
-            return;
-        }
-
-// Read some data! Most have just one port (port 0).
-        UsbSerialPort port = driver.getPorts().get(0);
-        try {
-            port.open(connection);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            port.setParameters(9600, 8, 1, 0);
-            byte buffer[] = new byte[16];
-            int numBytesRead = port.read(buffer, 1000);
-            Log.d("main", "Read " + numBytesRead + " bytes.");
-            byte[] test = new byte[]{0};
-            port.write(test, 100);
-        } catch (IOException e) {
-            Log.d("main", "Error");
-        } finally {
-            try {
-                port.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                if (mRecorder != null) {
+                    mRecorder.quit();
+                    mRecorder = null;
+                } else {
+                    Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+                    startActivityForResult(captureIntent, REQUEST_CODE);
+                }
             }
+        });
+
+        Button send = (Button) findViewById(R.id.bt_send);
+        EditText messageView = (EditText) findViewById(R.id.et_message);
+        final String message = messageView.getText().toString();
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebSocketClient.send(message);
+
+            }
+        });
+
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection == null) {
+            Log.e("@@", "media projection is null");
+            return;
+        }
+        // video size
+        final int width = 210;
+        final int height = 90;
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "record-" + width + "x" + height + "-" + System.currentTimeMillis() + ".mp4");
+        final int bitrate = 6000000;
+        mRecorder = new ScreenRecorder(width, height, bitrate, 1, mediaProjection, file.getAbsolutePath());
+        mRecorder.start();
+
+        Toast.makeText(this, "Screen recorder is running...", Toast.LENGTH_SHORT).show();
+        moveTaskToBack(true);
+    }
+
+    private void connectWebSocket(String ip) {
+        URI uri;
+        try {
+            uri = new URI("ws://" + ip);
+            Log.i("Websocket", "Connection to " + ip);
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
         }
 
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+                wsOpen = true;
+            }
 
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                Log.i("Websocket", "Message " + s);
+
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+                wsOpen = false;
+
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+                wsOpen = false;
+
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mRecorder != null) {
+            mRecorder.quit();
+            mRecorder = null;
+        }
     }
 }
